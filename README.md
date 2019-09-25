19beecd60478b7c2c4a8a78c083bb20cd4587772
Julien Tourteau <tourteaujulien@gmail.com>
2019-09-26 00:26:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Gitlab extracter
+
+The aim of this project is to offer a simple interface to extract data from a gitlab project.
+
+## Implementation
+
+This project is currently issuing HTTP GET requests to gitlab API in order to retrieve project's content.
+
+## Content
+
+The project currently consist in a simple script allowing to specify project's id to get associated issues.
+
+
+    Usage: ./retrieve_content.sh --id PROJECT_ID [-h|--help] [-a|--auth KEY] [-b|--browse] [-i|--id PROJECT_ID] [-o|--output OUTPUT] [-u|--url URL] 
+ 		-h|--help                       : Print help.
+ 		-a|--auth                       : Authentication key 
+ 		-b|--browse                     : Open JSON content with default web browser. 
+ 		-i|--id                         : Gitlab project id. 
+ 		-o|--output                     : Save JSON content into given file. 
+ 		-u|--url                        : Gitlab server URL ('https://gitlab.com' as default value).
+
+
+    Retrieve gitlab project's content as JSON file. 
+              	The content is either stored in a file or printed on standard output. 
+              	The JSON content can also be oppened into a web browser.
+             	
+              	NOTE : 'curl' package has to be installed.
+
+## Authentication
+
+Currently, the only supported authentication method is using **Personal access tokens**.
+
+Please refer to [Gitlab API documentation]: https://docs.gitlab.com/ee/api/#personal-access-tokens in order to configure acces to private projects.
+
+## More to come...
---
+++ retrieve_content.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+AUTH=
+BROWSE=false
+ID=
+KEY=
+OUTPUT=
+URL=https://gitlab.com
+BROWSE_FILE=
+DESCRIPTION="Retrieve gitlab project's content as JSON file. \n \
+             	The content is either stored in a file or printed on standard output. \n \
+             	The JSON content can also be oppened into a web browser.
+             	\n \
+             	NOTE : 'curl' package has to be installed."
+GITLAB_API=api/v4/projects
+USAGE="Usage: ${0} --id PROJECT_ID [-h|--help] [-a|--auth KEY] [-b|--browse] [-i|--id PROJECT_ID] [-o|--output OUTPUT] [-u|--url URL] \n \
+\t\t-h|--help                       : Print help.\n \
+\t\t-a|--auth                       : Authentication key \n \
+\t\t-b|--browse                     : Open JSON content with default web browser. \n \
+\t\t-i|--id                         : Gitlab project id. \n \
+\t\t-o|--output                     : Save JSON content into given file. \n \
+\t\t-u|--url                        : Gitlab server URL ('${URL}' as default value).\n\n"
+
+fail()
+{
+	echo "ERROR: $*" >&2
+	exit 1
+}
+
+step()
+{
+	len=$(echo "$*" | awk '{print length}')
+	echo
+	seq -s'#' 0 ${len} | tr -d '[:digit:]'
+	echo "$*"
+	seq -s'#' 0 ${len} | tr -d '[:digit:]'
+}
+
+#######################
+### Check arguments ###
+#######################
+[ ${#} -gt 0 ] || { printf "${USAGE}"; exit 1; }
+
+while [ ${#} -ne 0 ]; do
+	case "${1}" in
+		-h|--help)                  printf "%b\n%b\n" "${USAGE}" "${DESCRIPTION}"; exit 0;;
+		-a|--auth)                  shift; KEY=${1};;
+		-b|--browse)                shift; BROWSE=true;;
+		-i|--id)                    shift; ID=${1};;
+		-o|--output)                shift; OUTPUT=${1}.json;;
+		-u|--url)                   shift; URL=${1};;
+		*)                          printf "${USAGE}"; exit 1;;
+	esac
+	shift
+done
+
+[ -z "${ID}" ] && fail "You shall specify gitlab project id."
+[ -z "${KEY}" ] || AUTH=?private_token=${KEY}
+
+###################################
+### Check that URL is reachable ###
+###################################
+step "### Check that ${URL} is reachable"
+ping -c1 -W1 $(sed 's:^.*\://\(.*$\):\1:' <<< ${URL}) > /dev/null 2>&1 && echo "OK" || fail "Error : ${URL} is not reachable"
+
+########################
+### Retrieve content ###
+########################
+step "Retrieve project's issues"
+TMP_FILE=$(mktemp /tmp/gitlab-extracter.XXXXXX.json)
+curl -s ${URL}/${GITLAB_API}/${ID}/issues${AUTH} > ${TMP_FILE} && echo "OK" || fail "Error : Unable to retrieve project's content"
+
+########################
+### Retrieve content ###
+########################
+[ -z "${OUTPUT}" ] || { step "Saving JSON to ${OUTPUT}"; cp ${TMP_FILE} ${OUTPUT} && echo "OK" || echo "Unable to save file to ${OUTPUT}"; }
+
+#####################
+### Print content ###
+#####################
+[ -z "${OUTPUT}" ] && BROWSE_FILE=${TMP_FILE} || BROWSE_FILE=${OUTPUT}
+${BROWSE} && { sensible-browser ${BROWSE_FILE} || fail "Error : Unable to open file with default web browser"; } || [ -z "${OUTPUT}" ] && cat ${BROWSE_FILE}
+
+exit 0
+
+
+
+
+
+
